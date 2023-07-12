0924b23e4a9c5c2d4e817379e6482fbfd15a74c0
AnasNemrawi <anasnemrawi3000@gmail.com>
2023-07-12 22:18:04 +0000

    lab 12

---
+++ system/fire-events.js
@@ -0,0 +1,23 @@
+'use strict';
+
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+const host = `http://localhost:${port}`;
+const faker = require('faker');
+
+const io = require('socket.io-client');
+const socket = io.connect(host);
+
+setInterval(() => {
+    let flight = {
+        event: 'new-flight',
+        time: new Date(),
+        Details: {
+            airLine: 'Royal Jordanian Airlines',
+            flightID: faker.datatype.uuid(),
+            pilot: faker.name.firstName(),
+            destination: faker.address.city(),
+        },
+    };
+    socket.emit('light');
+}, 10000)
